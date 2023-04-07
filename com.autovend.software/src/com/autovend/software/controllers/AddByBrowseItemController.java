1fb0a73b7258b027eac6497ba25fb20d34a7156c
MohammedH41 <122569808+MohammedH41@users.noreply.github.com>
2023-04-07 02:25:12 -0600

    update

--- com.autovend.software/src/com/autovend/software/controllers/AddByBrowseItemController.java
+++ com.autovend.software/src/com/autovend/software/controllers/AddByBrowseItemController.java
@@ -21,23 +21,20 @@ public class AddByBrowseItemController extends ItemAdderController<TouchScreen, 
 	List<BarcodedProduct> browsedItem = new ArrayList<BarcodedProduct>();
 	BarcodedUnit barcodedUnit;
 	BigDecimal itemPrice;
-	BigDecimal itemTotalPrice;
 	BarcodedProduct barcodedItem;
     double itemWeight;
     
 	/*
 	 * get browsed Item
-	 * parameter(item name, quantity of items) get these from gui
-	 * set the value of barcoded item and the price (Barcode, expected weight)
+	 * parameter(item name, quantity of items)
+	 * 
 	 */
-	public void SelecetedProduct(String itemName, String quantity) {
-		//store value of quantity wanted in var
-		BigDecimal itemQuntity = new BigDecimal(quantity);
+	public void SelecetedProduct(String itemName) {
 		for(BarcodedProduct product : ProductDatabases.BARCODED_PRODUCT_DATABASE.values()) {
+			//if product exist in database
 			if(product.getDescription().equals(itemName)) {
 				barcodedItem = product;
 				itemPrice = product.getPrice();	//price of item 
-				itemTotalPrice = product.getPrice().multiply(itemQuntity); //item price x quantity 
 				itemWeight = product.getExpectedWeight(); //item weight 
 			}
 		}		
@@ -48,35 +45,23 @@ public class AddByBrowseItemController extends ItemAdderController<TouchScreen, 
 	 * Event When item is added by browsing 
 	 */
 	public void AddByBrowsingEvent(String selectedItem, String SelectedQuantity) {
-		
-		//display visual catalog (gui)
-			//selects product name (gui?) return itemName
 		String item = selectedItem; 
-		
-		
-			//and product quantity (gui?) return quantity
-		String quantity = SelectedQuantity;
-		
-		//cancel browse option (gui)
-			//return user back to original state prior to catalog screen 
-		
-		
 		disableDevice();  // Blocks the self-checkout system from further customer interaction.
-		//run the selectedProduct method to get info 
-		SelecetedProduct(item, quantity);
 		
+		SelecetedProduct(item);
+		
+		//if inputed item is not null
 		if (barcodedItem != null) {
+			//add item to bagging area and update expected weight 
 			this.getMainController().addItem(this, barcodedItem, itemWeight);
-			
+	
 		}
+		//if item added does not exit through exception
 		else {
-			throw new SimulationException("Null Product");
+			throw new SimulationException("Product does not exist");
 		}
-		
-		//signal to customer to place item in baggin area (gui)
 		
 		enableDevice();	//Unblocks the self-checkout system
-		//return back to state prior to entering catalog (gui) 
 	}
 	
 
