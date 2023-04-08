ec0a1e1c17ca4df14470c39c5cc163b3376eb694
aishanirfan <aishanirfan.na@ucalgary.ca>
2023-04-07 22:29:08 -0600

    adding the panel for the payment sub screen

--- com.autovend.software/src/com/autovend/software/gui/PaymentScreen.java
+++ com.autovend.software/src/com/autovend/software/gui/PaymentScreen.java
@@ -2,18 +2,18 @@ package com.autovend.software.gui;
 
 //Necessary imports
 import javax.swing.JFrame;
+
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+
+
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.Color;
 import java.awt.Dimension;
 
-import javax.swing.JPasswordField;
 import javax.swing.JLabel;
-import javax.swing.JLayeredPane;
 import javax.swing.JButton;
 
 
@@ -23,7 +23,6 @@ public class PaymentScreen {
 	
 	//all the components that are added to the screen
 	private JFrame touchScreenFrame1;
-	private JLayeredPane lPane;
 	private JPanel paymentPanel;
 	private JLabel label;
 	private JButton cash;
@@ -36,14 +35,89 @@ public class PaymentScreen {
 		touchScreenFrame1 = new JFrame();
 		touchScreenFrame1.setSize(new Dimension(1000,900));
 		
-		lPane = new JLayeredPane();
-		lPane.setSize(new Dimension(985,785));
+		
 		
 		paymentPanel = new JPanel();
 		paymentPanel.setSize(new Dimension(985,785));
+		paymentPanel.setLayout(null);
+		
+		
+		label = new JLabel("Please select a payment option:");
+		label.setFont(new Font("Times New Roman", Font.BOLD, 18));
+		label.setBounds(368, 200, 349, 54);
+		paymentPanel.add(label);
+		
+		cash = new JButton("Cash");
+		cash.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		cash.setBounds(405, 266, 171, 38);
+		cash.addActionListener(new ActionListener(){
+
+			public void actionPerformed(ActionEvent e) {
+				//implement the button press
+				System.out.println("Paying using cash");
+				
+			}
+			
+		});
+		paymentPanel.add(cash);
+		
+		
+		
+		credit = new JButton("Credit");
+		credit.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		credit.setBounds(405, 324, 171, 38);
+		credit.addActionListener(new ActionListener(){
+
+			public void actionPerformed(ActionEvent e) {
+				//implement the button press
+				System.out.println("Paying using Credit Card");
+				
+			}
+			
+		});
+		paymentPanel.add(credit);
+		
+		
+		debit = new JButton("Debit");
+		debit.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		debit.setBounds(405, 384, 171, 38);
+		debit.addActionListener(new ActionListener(){
+
+			public void actionPerformed(ActionEvent e) {
+				//implement the button press
+				System.out.println("Paying using Debit Card");
+			}
+			
+		});
+		paymentPanel.add(debit);
+		
+		
+		giftCard = new JButton("Gift Card");
+		giftCard.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		giftCard.setBounds(405, 451, 171, 38);
+		giftCard.addActionListener(new ActionListener(){
+
+			public void actionPerformed(ActionEvent e) {
+				//implement the button press
+				System.out.println("Paying using Gift Card");
+				
+			}
+			
+		});
+		paymentPanel.add(giftCard);
+		
+		
+		touchScreenFrame1.getContentPane().add(paymentPanel);
+		touchScreenFrame1.setVisible(true);
+		touchScreenFrame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		touchScreenFrame1.setResizable(false);
+
 		
 		
 		
 	}
-
+	
+	public static void main(String[]args) {
+		new PaymentScreen();
+	}
 }
