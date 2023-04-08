2abbb0376f945f1fa9817950605ffc0c79f7f54f
Zawad Safwan <Zawad Safwan@LAPTOP-UR0VF91F.mshome.net>
2023-04-07 22:06:09 -0600

    Added some new test case for remove attendant method

--- com.autovend.software.test/src/com/autovend/software/test/AttendantIDTest.java
+++ com.autovend.software.test/src/com/autovend/software/test/AttendantIDTest.java
@@ -26,119 +26,125 @@ public class AttendantIDTest{
 	SelfCheckoutStation station = new SelfCheckoutStation(c, BillDenomiantions, coinDenominations, 1000000, 1);
 	
 	/** Log In Test
-	 * Test 1: Failed to Log in as the user does not exist 
-	 * Test 2: Successful Log in with correct credentials
+	 * Test 1: Trying to Log in with Wrong Credentials 
+	 * Test 2: Successful Login after Adding a new Attendant 
 	 * Test 3: Check what happens when someone tries to log in again when another attendant is logged in
-	 * Test 4: Wrong Password  Failed Log In
-	 * Test 5 : Successful Login for super attendant **/
+	 * Test 4: Wrong Password for a User ID
+	 * Test 5: Successful Login for super attendant 
+	 * Test 6: Test Remove Attendant Function **/
 	
 	
-	
-	// Failed to Log in as the user does not exist
+	// Test 1: Trying to Log in with Wrong Credentials
 	@Test (expected = SimulationException.class)
-	public void Failed_Log_in() {
+	public void Failed_Log_in_WrongCreds() {
 		// Creating Station Instance for Test 1
-		CheckoutController stationTest1= new CheckoutController(station);
-		// Adding New Attendant
-		stationTest1.Attendant.add_attendant("Samuel", "oper14xj65");
+		CheckoutController stationTestLogin= new CheckoutController(station);
+		
 		// Calling Log in
-		stationTest1.Log_in_Attendant("Tom", "3523rq12ui3r");
+		stationTestLogin.Log_in_Attendant("Tom", "3523rq12ui3r");
 		
 	}
 	
-	// Successful Log in with correct credentials
+	// Test 2: Successful Login after Adding a new Attendant 
 	@Test 
-	public void Successful_Log_in(){
+	public void AddAttendtant_Succesful_Login(){
 		// Creating Station Instance for Test 1
-		CheckoutController stationTest2= new CheckoutController(station);
+		CheckoutController stationTestLogin= new CheckoutController(station);
 		// Adding New Attendant
-		stationTest2.Attendant.add_attendant("Samuel", "oper14xj65");
+		stationTestLogin.Attendant.add_attendant("Samuel", "oper14xj65");
 		// Calling Log in
-		stationTest2.Log_in_Attendant("Samuel", "oper14xj65");
-		assertTrue(stationTest2.Log_in_Status);
+		stationTestLogin.Log_in_Attendant("Samuel", "oper14xj65");
+		//Check Log_in_Status
+		assertTrue(stationTestLogin.Log_in_Status);
 		
 	}
 	
 
-	//Check when the user is already logged in Fails
+	// Test 3: Trying to Log in Multiple Times
 	@Test (expected = SimulationException.class)
 	public void Overlapping_Log_in(){
 		// Creating Station Instance for Test 1
-		CheckoutController stationTest3= new CheckoutController(station);
+		CheckoutController stationTestLogin= new CheckoutController(station);
 		// Adding New Attendant
-		stationTest3.Attendant.add_attendant("Samuel", "oper14xj65");
-		stationTest3.Attendant.add_attendant("Babar", "52618");
+		stationTestLogin.Attendant.add_attendant("Samuel", "oper14xj65");
+		stationTestLogin.Attendant.add_attendant("Babar", "52618");
 		
 		// Calling Log in
-		stationTest3.Log_in_Attendant("Samuel", "oper14xj65");
-		stationTest3.Log_in_Attendant("Babar", "52618");
+		stationTestLogin.Log_in_Attendant("Samuel", "oper14xj65");
+		stationTestLogin.Log_in_Attendant("Babar", "52618");
 		
 		
 	}
+	// Test 4: Real UserID with wrong password
 	@Test (expected = SimulationException.class)
 	public void Failed_Log_in_WrongPassword() {
 		// Creating Station Instance for Test 1
-		CheckoutController stationTest1= new CheckoutController(station);
+		CheckoutController stationTestLogin= new CheckoutController(station);
 		// Adding New Attendant
 		// Calling Log in
 		// inputting wrong password for James
-		stationTest1.Log_in_Attendant("James", "331");
+		stationTestLogin.Log_in_Attendant("James", "331");
 	}
 	
+	// Test 5: Real UserID with wrong password
 	@Test 
-	public void Successful_Log_in_2(){
+	public void Successful_Log_in_CurrentAttendant(){
 		// Creating Station Instance for Test 1
-		CheckoutController stationTest2= new CheckoutController(station);
+		CheckoutController stationTestLogin= new CheckoutController(station);
 		// Adding New Attendant
 		// Log in Attendant
-		stationTest2.Log_in_Attendant("James", "4628");
-		assertTrue(stationTest2.Log_in_Status);
+		stationTestLogin.Log_in_Attendant("James", "4628");
+		assertTrue(stationTestLogin.Log_in_Status);
+	}
+	
+	// Test 6: Test Remove Attendant Method
+	@Test (expected = SimulationException.class)
+	public void Remove_CurrentAttendant_FailLogin(){
+		// Creating Station Instance for Test 1
+		CheckoutController stationTestLogin= new CheckoutController(station);
+		// Adding New Attendant
+		// Log in Attendant
+		stationTestLogin.Attendant.remove_attendant("James", "4628");
+		stationTestLogin.Log_in_Attendant("James", "4628");
 	}
 	
 	
 	/** Log Out Test
 	 * Test 1: No accounts are logged in
-	 * Test 2: Successful Logout ---> Checks is Attendant ID is null
+	 * Test 2: Successful Logout ---> Checks if Attendant ID is null
 	 * Test 3: Successful Logout ----> Checks if Login Status is false**/
 	
 	//No accounts are logged in
 	@Test (expected = SimulationException.class)
 	public void Failed_Log_Out() {
-		CheckoutController stationTest4= new CheckoutController(station);
-		// Adding New Attendant
-		stationTest4.Attendant.add_attendant("Samuel", "oper14xj65");
-		stationTest4.Attendant.add_attendant("Babar", "52618");
-		
+		CheckoutController stationLogOut= new CheckoutController(station);	
 		// Calling Log out when no one is logged in
-		stationTest4.Log_Out_Attendant();
+		stationLogOut.Log_Out_Attendant();
 		
 	}
 	// Checks if the current attendant is null after logging out
 	@Test 
 	public void Succesful_Log_Out_2() {
-		CheckoutController stationTest4= new CheckoutController(station);
-		// Adding New Attendant
-		stationTest4.Attendant.add_attendant("Samuel", "oper14xj65");
-		stationTest4.Attendant.add_attendant("Babar", "52618");
-		// Logged in as James
-		stationTest4.Log_in_Attendant("James", "4628");
+		CheckoutController stationLogOut= new CheckoutController(station);
+		
+		// Logged in as James [Super Attendant]
+		stationLogOut.Log_in_Attendant("James", "4628");
 		// Calling Log out when no one is logged in
-		stationTest4.Log_Out_Attendant();
-		assertNull(stationTest4.Attendant_ID);
+		stationLogOut.Log_Out_Attendant();
+		assertNull(stationLogOut.Attendant_ID);
 	}
 	
 	//Successful Log Out
 	@Test
 	public void Succesfull_Log_Out() {
-		CheckoutController stationTest4= new CheckoutController(station);
+		CheckoutController stationLogOut= new CheckoutController(station);
 		// Adding New Attendant
-		stationTest4.Attendant.add_attendant("Samuel", "oper14xj65");
-		stationTest4.Attendant.add_attendant("Babar", "52618");
-		
+		stationLogOut.Attendant.add_attendant("Babar", "52618");
 		// Calling Log out when no one is logged in
-		stationTest4.Log_in_Attendant("Babar", "52618");
-		stationTest4.Log_Out_Attendant();
-		assertFalse(stationTest4.Log_in_Status);
+		stationLogOut.Log_in_Attendant("Babar", "52618");
+		//Logging Out
+		stationLogOut.Log_Out_Attendant();
+		assertFalse(stationLogOut.Log_in_Status);
 		
 	}
 	
