e535fa0571210b5f06e6e0d94ac9091b87a2fe88
AlanAyy <58347931+AlanAyy@users.noreply.github.com>
2023-04-09 16:16:50 -0400

    Updated SuspendController for Haoping

--- com.autovend.software.test/src/com/autovend/software/test/SuspendControllerTest.java
+++ com.autovend.software.test/src/com/autovend/software/test/SuspendControllerTest.java
@@ -1,39 +1,26 @@
 package com.autovend.software.test;
 import com.autovend.software.controllers.SuspendController;
 
-import org.junit.Before;
 import org.junit.Test;
+
+import sevenWonder.SuspendController.Station;
+
 import static org.junit.Assert.*;
 
 public class SuspendControllerTest {
-    private SuspendController station;
 
-    @Before
-    public void setUp() {
-        station = new SuspendController();
-    }
+    private SuspendController suspendController;
 
-    @Test
-    public void testInitialSuspendedStatus() {
-        assertTrue(station.isSuspended());
-    }
+
+
 
     @Test
-    public void testSuspend() {
-        station.suspend();
-        assertTrue(station.isSuspended());
+    public void testStationSetUp() {
+        SuspendController suspendController = new SuspendController(null);
+        Station testStation = suspendController.new Station("Test Station" , false);
+        suspendController = new SuspendController(testStation);
+        suspendController.stationSetUp();
+        assertTrue(suspendController.isSuspended());
     }
 
-    @Test
-    public void testUnsuspend() {
-        station.suspend();
-        station.unsuspend();
-        assertFalse(station.isSuspended());
-    }
-
-    @Test
-    public void testUnsuspendWhenNotSuspended() {
-        station.unsuspend();
-        assertFalse(station.isSuspended());
-    }
 }
--- com.autovend.software/src/com/autovend/software/controllers/SuspendController.java
+++ com.autovend.software/src/com/autovend/software/controllers/SuspendController.java
@@ -7,10 +7,11 @@ import java.util.Scanner;
 public class SuspendController {
     public Station station;
     public boolean suspended; // Indicates if the station is currently suspended
+
     //Create a Station class for suspendController test.
-    class Station {
+    public class Station {
         private String name;
-        private boolean inUse;
+        private boolean inUse = false;
 
         public Station(String name, boolean inUse) {
             this.name = name;
@@ -33,8 +34,12 @@ public class SuspendController {
 
     // Constructor
     public SuspendController(Station station) {
-        this.station = station;
-        this.station.inUse=station.inUse;//set the inuse situation as false.
+        if (station == null) {
+            this.station = new Station("", false);
+        } else {
+            this.station = station;
+            this.station.inUse = station.inUse;
+        }
         this.suspended = false; // Initialize the station as not suspended before anything happens.
     }
 
@@ -44,16 +49,19 @@ public class SuspendController {
     }
 
     // Method to suspend the station
-    public void suspend() {
+    public boolean suspend() {
         if (!suspended && !station.isInUse()) {
             // Check if the station is not already suspended and not in use
             System.out.println("The Station has been suspended.");
             suspended = true;
+            return suspended;
         } else if (suspended) {
             System.out.println("The Station is already suspended.");
+            return suspended;
         } else {
             //The system won't be suspended when user is in use.
             System.out.println("Cannot suspend the station when customer is using it.");
+            return suspended;
         }
     }
 
@@ -77,58 +85,83 @@ public class SuspendController {
     public static void main(String[] args) {
         // Create a list of stations
         List<SuspendController> stations = new ArrayList<>();
-        Station Station1 = new Station("Station1" , true);
-        Station Station2 = new Station("Station2" , false);
-        Station Station3 = new Station("Station3" , true);
-
+        SuspendController suspendController1 = new SuspendController(null);
+        Station Station1 = suspendController1.new Station("Station1" , true);
+        SuspendController suspendController2 = new SuspendController(null);
+        Station Station2 = suspendController2.new Station("Station2" , false);
+        SuspendController suspendController3 = new SuspendController(null);
+        Station Station3 = suspendController3.new Station("Station3" , true);
         stations.add(new SuspendController(Station1));
         stations.add(new SuspendController(Station2));
         stations.add(new SuspendController(Station3));
 
 
+
+
         //create a scanner class for
-        Scanner scanner = new Scanner(System.in);
-        // Attendant I/O: Display the list of stations for selection
-        System.out.println("Select a station to suspend/unsuspend:");
-        for (int i = 0; i < stations.size(); i++) {
-            System.out.println((i + 1) + ". Station " + (i + 1));
-        }
-
+        int selectedStation = 0;
         // Attendant I/O: Get the selected station
-        int selectedStation = 1; // Example: Attendant selects station 1
-        System.out.println("Attendant selected Station " + selectedStation);
-
-        //while loop for testing user input, if user enter suspend, can be changed when we apply GUI in the system.
+        bigLoop:
         while (true) {
-            System.out.println("Enter 'suspend' to suspend the process or 'unsuspend' to unsuspend the process:");
-            String input = scanner.nextLine();
+            Scanner scanner = new Scanner(System.in);
+            // Attendant I/O: Display the list of stations for selection
+            System.out.println("Select a station to suspend/unsuspend:");
+            for (int i = 0; i < stations.size(); i++) {
+                System.out.println((i + 1) + ". Station " + (i + 1));
+            }
+            if (scanner.hasNextInt()) {
+                selectedStation = scanner.nextInt();
+                System.out.println("Attendant selected Station " + selectedStation);
+            } else {
+                scanner.nextLine(); // consume invalid input
+                System.out.println("Invalid input. Please enter an integer.");
+                continue bigLoop;
+            }
 
-            if (input.equalsIgnoreCase("suspend")) {
-                //invoke station that is getting suspended, in this case its station no.1 .
-                SuspendController stationToSuspend = stations.get(selectedStation - 1);
-                stationToSuspend.suspend();
-            } else if (input.equalsIgnoreCase("unsuspend")) {
-                // System: Communicate with the Customer I/O to unsuspended the selected station
-                try {
-                    SuspendController stationToUnsuspend = stations.get(selectedStation - 1);
-                    if (stationToUnsuspend.isSuspended()) {
-                        stationToUnsuspend.unsuspend();
-                        // Customer I/O: Ready for further customer interaction
-                        System.out.println("Station " + selectedStation + " is ready for customer interaction.");
-                    } else {
-                        System.out.println("Station " + selectedStation + " is not currently suspended.");
+
+
+
+
+            //while loop for testing user input, if user enter suspend, can be changed when we apply GUI in the system.
+            while (true) {
+                System.out.println("Enter 'suspend' to suspend the process or 'unsuspend' to unsuspend the process:");
+                System.out.println("Enter 'exit' to exit the process or 'change' to pick another station:");
+                String input = scanner.nextLine();
+
+                if (input.equalsIgnoreCase("suspend")) {
+                    //invoke station that is getting suspended, in this case its station no.1 .
+                    SuspendController stationToSuspend = stations.get(selectedStation - 1);
+                    stationToSuspend.suspend();
+                } else if (input.equalsIgnoreCase("unsuspend")) {
+                    // System: Communicate with the Customer I/O to unsuspended the selected station
+                    try {
+                        SuspendController stationToUnsuspend = stations.get(selectedStation - 1);
+                        if (stationToUnsuspend.isSuspended()) {
+                            stationToUnsuspend.unsuspend();
+                            // Customer I/O: Ready for further customer interaction
+                            System.out.println("Station " + selectedStation + " is ready for customer interaction.");
+                        } else {
+                            System.out.println("Station " + selectedStation + " is not currently suspended.");
+                        }
+                    } catch (IndexOutOfBoundsException e) {
+                        System.out.println("Invalid selection. Please try again.");
+                    } catch (Exception e) {
+                        System.out.println("Failed to unsuspend the station due to an error: " + e.getMessage());
                     }
-                } catch (IndexOutOfBoundsException e) {
-                    System.out.println("Invalid selection. Please try again.");
-                } catch (Exception e) {
-                    System.out.println("Failed to unsuspend the station due to an error: " + e.getMessage());
+                }
+                else if(input.equalsIgnoreCase("exit")) {
+                    System.out.println("Exit successfully.");
+                    break bigLoop;
+                }
+                else if(input.equalsIgnoreCase("change")) {
+                    System.out.println("Please pick another station.");
+                    continue bigLoop;
+                }
+                else {
+                    System.out.println("Invalid input. Please try again.");
                 }
             }
-            else {
-                System.out.println("Invalid input. Please try again.");
-            }
         }
-
-
     }
 }
+
