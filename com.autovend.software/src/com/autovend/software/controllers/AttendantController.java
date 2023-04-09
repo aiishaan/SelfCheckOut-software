cb10d870a8b27ccc48474bc33b96518c2426e1c2
HaziqKhawaja <93850451+HaziqKhawaja@users.noreply.github.com>
2023-04-08 19:47:03 -0600

    Added two new methods

---
+++ com.autovend.software/src/com/autovend/software/controllers/AttendantController.java
@@ -0,0 +1,68 @@
+package com.autovend.software.controllers;
+import java.util.HashMap;
+
+public class AttendantController {
+	// Handles the Operations of the attendant
+	
+	// Attendant user ID
+	public String user_id;
+	// Attendant Password
+	public String password;
+	
+	// Attendant Lists for the Software
+	public HashMap<String, String> AttendantList = new HashMap<String, String>();
+	
+	
+	
+	/** Constructs an Attendant with user ID and Password 
+	 * 
+	 * @param username
+	 * @param key
+	 */
+	
+	public AttendantController(String username, String pass) {
+		this.user_id= username;
+		this.password=pass;
+		// Adding Some Current Attendants to the List
+		AttendantList.put("James", "4628");
+		AttendantList.put("Wayne", "331");
+		AttendantList.put("Shaw", "unux89");
+		AttendantList.put("Pablo", "12345678");
+		
+	}
+	
+	/** Adds the userID and password for a new attendant
+	 * 
+	 * 
+	 * **/
+	public void add_attendant(String username, String password) {
+		
+		AttendantList.put(username, password);
+		
+	}
+	
+	/**Method to remove attendant
+	 * 
+	 * **/
+	
+	public void remove_attendant(String username, String pass) {
+		
+		AttendantList.remove(username, pass);
+	}
+	
+	
+	// Getter method for Attendant ID
+	public String getUser_id() {
+		return user_id;
+	}
+	
+	
+	public void attendantStationStartup(CheckoutController checkoutController) {
+		checkoutController.stationStartup();
+	}
+	
+	public void attendantStationShutdown(CheckoutController checkoutController) {
+		checkoutController.stationShutdown();
+	}
+
+}
