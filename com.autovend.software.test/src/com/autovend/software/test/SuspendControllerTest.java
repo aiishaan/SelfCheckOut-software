9e9d070bd99df9fd41fae7efc931167c2c829d9c
jfl007 <127908536+jfl007@users.noreply.github.com>
2023-04-09 22:32:47 -0600

    Update SuspendControllerTest.java
    Tests for unsuspended() method have been made, @Before clause has been added.

--- com.autovend.software.test/src/com/autovend/software/test/SuspendControllerTest.java
+++ com.autovend.software.test/src/com/autovend/software/test/SuspendControllerTest.java
@@ -1,22 +1,32 @@
 package com.autovend.software.test;
+
 import com.autovend.software.controllers.SuspendController;
-
 import org.junit.Test;
-
+import org.junit.Before;
 import com.autovend.software.controllers.SuspendController.Station;
-
 import static org.junit.Assert.*;
 
 public class SuspendControllerTest {
 
     private SuspendController suspendController;
 
+    /**
+	 * Set up work station and suspendController object 
+	 */
+    @Before 
+    public void setUpTests() {
+    	suspendController = new SuspendController(null);
+    	Station station = suspendController.new Station("Test Station" , false);
+    	suspendController = new SuspendController(station);
+    	suspendController.stationSetUp(); 
+    }
+    
+    /**
+     * Test if the method stationSeetUp() actually sets the field suspended to true 
+     * Expected: public field suspended should be true after calling this method 
+     */
     @Test
     public void testStationSetUp() {
-        SuspendController suspendController = new SuspendController(null);
-        Station testStation = suspendController.new Station("Test Station" , false);
-        suspendController = new SuspendController(testStation);
-        suspendController.stationSetUp();
         assertTrue(suspendController.isSuspended());
     }
 
@@ -50,10 +60,26 @@ public class SuspendControllerTest {
         suspendController.suspend();
         assertFalse(suspendController.isSuspended());
     }
-
-
-
-
-
+    
+     /**
+     * Test if the method unSuspend() sets the boolean value suspended to false if it is currently true 
+     * Expected: public field suspended should be false after calling this method 
+     */
+    @Test 
+    public void unSuspend_Suspended_Test() {
+    	suspendController.unsuspend();
+    	assertFalse(suspendController.isSuspended());
+    }
+    
+    /**
+     * Test if the method unSuspend() does not change the boolean value suspended if it is currently false 
+     * Expected: public field suspended should be false after calling this method 
+     */
+    @Test 
+    public void UnSuspend_NotSuspended_Test() {
+    	suspendController.suspended = false; 
+    	assertFalse(suspendController.isSuspended());
+    }
+    
 
 }
