d5b20e4812e3e6337409dee683e107feb1d13e30
jitaksha2001 <jitakshabatish2001@gmail.com>
2023-04-12 12:31:22 -0600

    Added Group Info!

--- com.autovend.software.test/src/com/autovend/software/test/AddByBrowseItemControllerTest.java
+++ com.autovend.software.test/src/com/autovend/software/test/AddByBrowseItemControllerTest.java
@@ -1,3 +1,35 @@
+/*
+SENG 300 Project Iteration 3
+Group 8
+
+UCID		Name
+10101894	Nicholas Flanagan-Bergeron
+30018276	Shijia(David) Wang
+30066080	Xinzhou Li
+30072318	Haoping Zheng
+30106232	Aman Hossain
+30113185	Mingyang Li
+30116450	Jitaksha Batish
+30116484	Rahat Chowdhury
+30118846	Aneel Parmar
+30127597	Nam Anh Vu
+30130139	Alan Alcocer-Iturriza
+30139344	Youssef Samaan
+30140581	Eyram Ekpe
+30141134	David Oti-George
+30141208	Tamerlan Ormanbayev
+30141335	Janet Tsegazeab
+30146181	Efren Garcia
+30148021	Haziq Khawaja
+30148838	Ryan Janiszewski
+30150496	Alireza Vafisani
+30150892	Abrar Zawad Safwan
+30151170	Jordan Tewnion
+30157742	Aishan Irfan
+30158563	Tyler Nguyen
+30159927	Aaron Tigley
+
+*/
 /**
  * 
  */
--- com.autovend.software/src/com/autovend/software/controllers/BillDispenserController.java
+++ com.autovend.software/src/com/autovend/software/controllers/BillDispenserController.java
@@ -1,18 +1,34 @@
 /*
-SENG 300 Project Iteration 2
-Group 7
-Niran Malla 30086877
-Saksham Puri 30140617
-Fatema Chowdhury 30141268
-Janet Tesgazeab 30141335
-Fabiha Fairuzz Subha 30148674
-Ryan Janiszewski 30148838
-Umesh Oad 30152293
-Manvi Juneja 30153525
-Daniel Boettcher 30153811
-Zainab Bari 30154224
-Arie Goud 30163410
-Amasil Rahim Zihad 30164830
+SENG 300 Project Iteration 3
+Group 8
+
+UCID		Name
+10101894	Nicholas Flanagan-Bergeron
+30018276	Shijia(David) Wang
+30066080	Xinzhou Li
+30072318	Haoping Zheng
+30106232	Aman Hossain
+30113185	Mingyang Li
+30116450	Jitaksha Batish
+30116484	Rahat Chowdhury
+30118846	Aneel Parmar
+30127597	Nam Anh Vu
+30130139	Alan Alcocer-Iturriza
+30139344	Youssef Samaan
+30140581	Eyram Ekpe
+30141134	David Oti-George
+30141208	Tamerlan Ormanbayev
+30141335	Janet Tsegazeab
+30146181	Efren Garcia
+30148021	Haziq Khawaja
+30148838	Ryan Janiszewski
+30150496	Alireza Vafisani
+30150892	Abrar Zawad Safwan
+30151170	Jordan Tewnion
+30157742	Aishan Irfan
+30158563	Tyler Nguyen
+30159927	Aaron Tigley
+
 */
 
 package com.autovend.software.controllers;
